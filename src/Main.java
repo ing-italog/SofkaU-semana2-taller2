aa402afa22dc25d6023a9d73cc325210abd66a68
Italo Guevara <94551786+ing-italog@users.noreply.github.com>
2022-02-16 16:45:02 -0500

    correpción de sintaxis

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import controller.Controller;
 /**
  * [Este es la clase principal , aquí arranca la aplicación, se le solicta al
  *  usuario que eliga una opción, seguido a esto se llama al controlar, el cual
- *  es el encargado de llamar a la clases de servicio]
+ *  es el encargado de llamar a la clase controller]
  * 
  * @version [1.0.0 2022-02-14]
  *
